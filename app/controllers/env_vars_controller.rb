57e86824f3a53ccf6a6b5c3f5e1977ea4a408c01
rsubramanian <Ramanathan.TirunellaiSubramanian@careerbuilder.com>
2013-05-02 08:59:12 -0400

    first commit

---
+++ app/controllers/env_vars_controller.rb
@@ -0,0 +1,17 @@
+class EnvVarsController < ApplicationController
+  def index
+    if Rails.env.development?
+      @env = ENV["WEBSVC_URL_DEV"]
+      @dev = ENV["PARTNERID_DEV"]
+    end
+    if Rails.env.test?
+      @env = ENV["WEBSVC_URL_TEST"]
+    end
+
+    if Rails.env.production?
+      @env =    ENV["WEBSVC_URL_PROD"]
+      @dev = ENV["PARTNERID_PROD"]
+    end
+    return @env
+  end
+end
